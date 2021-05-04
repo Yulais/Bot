435f9c32d7d52e4a4f32e8fcd7ec6207837754a2
Sherik <sherikhov.andrei@gmail.com>
2021-05-04 15:52:36 -0500

    aa

--- commands/user/ts.js
+++ commands/user/ts.js
@@ -8,20 +8,14 @@ module.exports = {
     run: async (bot, message, args) => {
         message.delete()
         let info1 = `Ts.astropvp.club`
-        let info2 = `_Yulais_#0009, 15wv#7773, 73#1543`
-        let info3 = `Dxpends#6096,Sherik#7117`
-        let info4 = `Sherik#7117`
 
         let embed = new MessageEmbed()
-            .setTitle(`Bot Information`)
+            .setTitle(`✸ | Astro Network`)
             .setColor(`DARK_RED`)
             .setThumbnail(bot.user.displayAvatarURL())
             .setFooter(message.guild.name)
             .setTimestamp()
-            .addField(`ts`, info1, true)
-            .addField(`Owners:`, info2, true)
-            .addField(`Developers:`, info3, true)
-            .setDescription(`Bot Developed by: ${info4}`)
+            .addField(`Team Speak`, info1, true)
         message.channel.send(embed).then(m => (m.delete({timeout: 1000000})));
     }
 }
