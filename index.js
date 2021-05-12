44b1936ba8906b16b1a0f1d1f21cc758d769d25a
Yulais <84100920+Yulais@users.noreply.github.com>
2021-05-12 14:46:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const { token } = require(`./botconfig.json`);
 const { prefix } = require(`./config.json`);
 [`aliases`, `commands`].forEach(x => bot[x] = new Collection());
 ["command", "events"].forEach(x => require(`./handlers/${x}`)(bot));
-bot.login(token);
+bot.login(ODMzODI0MDIyNjI1NTE3Njg5.YH39Ig.o7CHFAktgd3uaY364AWE2LcB9DI);
